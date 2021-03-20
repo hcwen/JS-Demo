37c062c9fc843aaa3f609abe583214222c5b7fc2
675754016@qq.com <31315495+hcwen@users.noreply.github.com>
2021-03-21 00:21:19 +0800

    add js demo

---
+++ 第一天-表单验证/javascript.js
@@ -0,0 +1,11 @@
+//get element
+const form = document.getElementById('from');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//event listener
+form.addEventListener('submit',function(){
+    console.log(123);
+})
